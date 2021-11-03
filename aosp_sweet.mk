3b3b21060a821c341502999289e4b78c348b69dc
zaidkhan0997 <kzaid0997@gmail.com>
2021-11-03 16:44:26 +0530

    sweet: BringUp PixelExtended Snow
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- aosp_sweet.mk
+++ aosp_sweet.mk
@@ -5,18 +5,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit common PixelPlusUi configurations
+# Inherit common PixelExtended configurations
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# PixelPlusUI Official Stuff
+# PixelExtended Official Stuff
 CUSTOM_BUILD_TYPE := OFFICIAL
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
 
 PRODUCT_NAME := aosp_sweet
 PRODUCT_DEVICE := sweet
