d820c41c6b41478d08ab3a269ef02096bd1bca74
zaidkhan0997 <kzaid0997@gmail.com>
2021-12-13 16:01:22 +0530

    sweet: switch to ventom kernel
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+git clone -b master --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
+git clone -b courbet --depth=1 https://github.com/vantoman/kernel_xiaomi_sm6150.git kernel/xiaomi/sweet 
