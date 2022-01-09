8d28c80db23362263823ba9ca0cbcd6a9e63d55f
zaidkhan0997 <kzaid0997@gmail.com>
2022-01-09 14:17:17 +0530

    sweet: update vendorsetup.sh
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,9 @@
+# Nuke
+rm -rf device/qcom/sepolicy_vndr
+rm -rf hardware/qcom-caf/sm8150/display
+
+# clone
+git clone -b twelve https://github.com/zaidkhan0997/hardware_qcom-caf_sm8150-common_display.git hardware/qcom-caf/sm8150/display
+git clone -b pex https://github.com/zaidkhan0997/android_device_qcom_sepolicy_vndr-1.git device/qcom/sepolicy_vndr
 git clone -b master --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
-git clone -b courbet --depth=1 https://github.com/vantoman/kernel_xiaomi_sm6150.git kernel/xiaomi/sweet 
+git clone -b courbet --depth=1 https://github.com/vantoman/kernel_xiaomi_sm6150.git kernel/xiaomi/sweet
