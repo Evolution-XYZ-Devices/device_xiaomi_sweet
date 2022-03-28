f38ba0739ca4ac8183afd190c019bbd758a1418b
zaidkhan0997 <kzaid0997@gmail.com>
2022-03-28 11:31:13 +0530

    sweet: Build more camera packages
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- device.mk
+++ device.mk
@@ -224,7 +224,14 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    android.hardware.camera.provider@2.4-external
+    android.hardware.camera.provider@2.4-external \
+    libmm-qcamera \
+    libdng_sdk.vendor \
+    libgui_vendor \
+    CameraGo \
+    libxml2 \
+    privapp-permissions-camera-go.xml \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_PACKAGES += \
     libshim_megvii
