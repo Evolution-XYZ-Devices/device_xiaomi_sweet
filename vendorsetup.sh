92d49005b44c4d4492e39c8059f4c13166a3e823
zaidkhan0997 <kzaid0997@gmail.com>
2022-03-30 23:48:45 +0530

    sweet: track sepolicy_vndr from arrow-12.0 branch
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,6 +4,6 @@ rm -rf hardware/qcom-caf/sm8150/display
 
 # clone
 git clone -b 19.1-sm8150 https://github.com/zaidkhan0997/android_hardware_qcom_display.git hardware/qcom-caf/sm8150/display
-git clone -b pex https://github.com/zaidkhan0997/android_device_qcom_sepolicy_vndr-1.git device/qcom/sepolicy_vndr
+git clone -b arrow-12.0 https://github.com/zaidkhan0997/android_device_qcom_sepolicy_vndr-1.git device/qcom/sepolicy_vndr
 git clone -b master --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
 git clone -b s12oss --depth=1 https://github.com/shashank1439/kernel_xiaomi_sweet.git kernel/xiaomi/sweet
