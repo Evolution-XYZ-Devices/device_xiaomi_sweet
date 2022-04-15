f82443cc5f452d1da0dc044219e69b7092283bfd
zaidkhan0997 <kzaid0997@gmail.com>
2022-04-15 09:14:42 +0530

    sweet: Bringup Evolution-X OFFICIAL
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sweet.mk
+    $(LOCAL_DIR)/evolution_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_sweet-user \
-    lineage_sweet-userdebug \
-    lineage_sweet-eng
+    evolution_sweet-user \
+    evolution_sweet-userdebug \
+    evolution_sweet-eng
