61f84bd209de3069f34169aad51847e426c9d3d7
zaidkhan0997 <kzaid0997@gmail.com>
2022-04-15 09:14:42 +0530

    sweet: Bringup Evolution-X OFFICIAL
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- evolution_sweet.mk
+++ evolution_sweet.mk
@@ -11,10 +11,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit common Evolution-X Stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Evolution-X Stuff
+EVO_BUILD_TYPE := OFFICIAL
+
+PRODUCT_NAME := evolution_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
