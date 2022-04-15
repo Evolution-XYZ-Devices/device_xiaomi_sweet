b9bbd6bf4d1d67b02722e976fd21612772bd2f0c
zaidkhan0997 <kzaid0997@gmail.com>
2022-04-15 11:04:03 +0530

    sweet: switch to vantom kernel
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+git clone -b 15 --depth=1 https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/clang-playground
+git clone -b courbet-12.1 --depth=1 https://github.com/vantoman/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
