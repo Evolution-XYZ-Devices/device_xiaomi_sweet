0752e5879ccd14f54cb2385e676dcdfe3fef389c
zaidkhan0997 <kzaid0997@gmail.com>
2022-07-08 06:25:31 +0530

    sweet: sync miuicamera from gitlab
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,3 @@
 git clone -b 15 --depth=1 https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/clang-playground
 git clone -b courbet-12.1 --depth=1 https://github.com/vantoman/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+git clone -b 12.1 https://gitlab.com/basamaryan/vendor_xiaomi_sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
